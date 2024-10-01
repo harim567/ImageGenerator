9306d07e61335c6d43a20584dda10c97c2ae8333
harim567 <harim0567@gmail.com>
2024-10-01 22:05:32 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from diffusers import DiffusionPipeline
+import torch
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.enable_model_cpu_offload()
+
+# if using torch < 2.0
+# pipe.enable_xformers_memory_efficient_attention()
+
+prompt = "An astronaut riding a green horse"
+
+images = pipe(prompt=prompt).images[0]
+st.image(images, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
